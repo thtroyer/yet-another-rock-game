bc0e39167f882ec62eb51d598492134a940049dc
Tom.Troyer <Tom.Troyer@cd9e3428-7879-449b-ab8d-2f768918fb74>
2010-02-06 00:22:46 +0000

    consts.h

---
+++ consts.h
@@ -0,0 +1,15 @@
+
+/*
+** YARG consts.h
+**	Copyright Tom Troyer 2010 (tom.troyer@gmail.com)
+** Released under GPLv3
+*/
+
+#ifndef _consts
+#define _consts
+
+const float deltaTimeConst = .005;
+
+const float pi = 3.1415926;
+
+#endif
