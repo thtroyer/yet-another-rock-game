6297e1bcc9d927ff8889e40c623aff259364e4dc
Tom.Troyer <Tom.Troyer@cd9e3428-7879-449b-ab8d-2f768918fb74>
2010-04-10 19:16:14 +0000

    Added WarpEffect (already implemented and used, but forgot to commit it a few revisions ago).

---
+++ WarpEffect.cpp
@@ -0,0 +1,110 @@
+
+#include "WarpEffect.h"
+
+WarpEffect::WarpEffect(){
+	x = 0;
+	y = 0;
+	age = 0;
+	radius = 10;
+	active = false;
+	Clock = new sf::Clock();
+	Clock->Reset();
+	//lastTime = 0;
+}
+
+WarpEffect::WarpEffect(float intX, float intY){
+	x = intX;
+	y = intY;
+	age = 0;
+	radius = 10;	
+}
+
+void WarpEffect::setAge(int mAge){
+	maxAge = mAge;
+	halfLife = maxAge/2;
+}
+
+void WarpEffect::setHalfLife(int mHalf){
+	halfLife = mHalf;
+	maxAge = halfLife * 2;
+}
+
+void WarpEffect::draw(){
+	//dRadius = 1/x ?
+	//Radius = ln(abs(age)) + C
+	
+	//std::cout << "Age: " << age << std::endl;
+	if(!active){
+		return;
+	}
+	
+	if(age>1500){
+		return;
+	}
+	
+	float opacity;
+	
+	radius = 5 * log(abs((age/5)-(halfLife/5))) + 4;
+	 
+	//opacity = 5/radius;
+	//if(opacity >= 1.0)
+	//	opacity = 1;
+	
+	opacity = -1/1500.0 * age + 1;
+	//std::cout << "op: " << opacity << std::endl;
+	
+	//opacity = 1.0;
+	
+	//std::cout << "Radius: " << radius << std::endl;
+	glColor4f(0.0, 0.1, 0.85, opacity);
+	glBegin(GL_LINE_LOOP);
+	for(float angle=0; angle<2*pi; angle+=.17){ //.17 ~= 10 degrees
+		glVertex2f(x + sin(angle) * radius, y + cos(angle) * radius);
+	}
+	glEnd();
+	
+	radius = 5 * log(abs(((age-100)/5)-(halfLife/5))) + 4;
+	
+	glBegin(GL_LINE_LOOP);
+	for(float angle=0; angle<2*pi; angle+=.17){ //.17 ~= 10 degrees
+		glVertex2f(x + sin(angle) * (radius), y + cos(angle) * (radius));
+	}
+	glEnd();
+
+	radius = 5 * log(abs(((age-300)/5)-(halfLife/5))) + 4;
+	
+	glBegin(GL_LINE_LOOP);
+	for(float angle=0; angle<2*pi; angle+=.17){ //.17 ~= 10 degrees
+		glVertex2f(x + sin(angle) * (radius), y + cos(angle) * (radius));
+	}
+	glEnd();
+}
+
+//void WarpEffect::incAge(Uint32 deltaTime){
+void WarpEffect::incAge(){
+	//deltaTime = SDL_GetTicks() - lastTime;
+	//lastTime = SDL_GetTicks();
+	deltaTime = (Clock->GetElapsedTime() * 1000) - lastTime;
+	lastTime = Clock->GetElapsedTime() * 1000;
+
+	//std::cout << "deltaTime: " << deltaTime << std::endl;
+	age += deltaTime;
+	//std::cout << "age: " << age << std::endl;
+}
+
+void WarpEffect::initiate(float intX, float intY, int inHalfLife){
+	age = 0;
+	x = intX;
+	y = intY;
+	halfLife = inHalfLife;
+	maxAge = halfLife * 2;
+	active = true;
+}
+
+float WarpEffect::getX(){
+	return x;
+}
+
+float WarpEffect::getY(){
+	return y;
+}
---
+++ WarpEffect.h
@@ -0,0 +1,63 @@
+#ifndef _WarpEffect
+#define _WarpEffect
+
+#if _WIN32
+#include <windows.h>
+#endif
+
+#include <iostream>
+#include <cstdlib>
+
+#include <GL/gl.h>
+
+//#include <SDL/SDL.h>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+#include <math.h>
+#include <time.h>
+
+#include "consts.h"
+
+
+class WarpEffect{
+	float x;
+	float y;
+	float age;
+	int opacity;
+	float radius;
+	int maxAge;
+	int halfLife;
+	
+	float deltaTime;
+	float lastTime;
+	
+	bool active;
+
+	sf::Clock* Clock;
+	
+	public:
+//	WarpEffect();
+
+
+	WarpEffect();    
+	WarpEffect(float intX, float intY);    
+	
+	//use either setAge or setHalfLife
+	//two ways to set length of effect
+	
+	void setAge(int);
+	void setHalfLife(int);
+
+	void draw();
+    
+//	void incAge(Uint32 deltaTime);
+	void incAge();
+	
+	void initiate(float intX, float intY, int inHalfLife);
+	
+	float getX();
+	float getY();
+};
+
+#endif
